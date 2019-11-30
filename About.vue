6c62ff90993c25eb3353064ffe9f71b300011f63
Jasonczx <55595850+Jasonczx@users.noreply.github.com>
2019-11-30 13:36:52 -0500

    Create About.vue

---
+++ About.vue
@@ -0,0 +1,58 @@
+// About.vue
+
+<template>
+    <div class="main">
+        <div class="title-about">
+            <h1>The Team</h1>
+        </div>
+        <div class="responsive">
+            <div class="person">
+                <img src="../assets/img/personplaceholder.png" alt="person1" width="600" height="400">
+                <div class="desc">
+                    <p>Name: Nancy Mai</p>
+                    <p>Role: Back end developer</p>
+                </div>
+            </div>
+        </div>
+        <div class="responsive">
+            <div class="person">
+                <img src="../assets/img/personplaceholder.png" alt="person2" width="600" height="400">
+                <div class="desc">
+                    <p>Name: Brandon Holmes</p>
+                    <p>Role: Front end developer</p>
+                </div>
+            </div>
+        </div>
+        <div class="responsive">
+            <div class="person">
+                <img src="../assets/img/personplaceholder.png" alt="person3" width="600" height="400">
+                <div class="desc">
+                    <p>Name: Sarah Rostami</p>
+                    <p>Role: Front end Developer</p>
+                </div>
+            </div>
+        </div>
+        <div class="responsive">
+            <div class="person">
+                <img src="../assets/img/personplaceholder.png" alt="person4" width="600" height="400">
+                <div class="desc">
+                    <p>Name: Zixuan Chen</p>
+                    <p>Role: Back end Developer</p>
+                </div>
+            </div>
+        </div>
+        <div class="ref-title">
+            <h1>References</h1>
+        </div>
+        <div class="ref">
+            <ul>
+                <li>list</li>
+                <li>here</li>
+            </ul>
+        </div>
+    </div>
+</template>
+
+<style lang="scss">
+  @import '../assets/css/about.css';
+</style>
